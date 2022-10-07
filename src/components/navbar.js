257b6896f856d3ee744c702c55810723498298e4
samsvk <samsvk00@gmail.com>
2022-10-07 18:21:12 +0100

    logo fix

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="flex flex-center_all flex-between">
       <button className="btn text">Menu</button>
-      <h1 className="text">433-football</h1>
+      <h1 className="text">4-3-3</h1>
       <a className="text">Contact Us</a>
     </nav>
   );
